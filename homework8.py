8288ea79fdd711a5fcfd74a05ad11f8348df4c7d
EvgeniySuharev <169095875+EvgeniySuharev@users.noreply.github.com>
2024-05-19 12:18:38 +0300

    Update homework8.py

--- homework8.py
+++ homework8.py
@@ -2,4 +2,4 @@ cars = ["BMW", "MB", "KIA", "HONDA"]
 cars_count = 0
 for i in range(len(cars)):
     cars_count += 10
-    print('Яезжу на автомобиле марки', cars[i], cars_count)
+    print('Я езжу на автомобиле марки', cars[i], cars_count)
